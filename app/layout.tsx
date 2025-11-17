3a8687ea79f3d8928198d5ece8bbb1f95900449f
Kate Liu <lhyhgshyd@163.com>
2025-11-17 15:37:44 -0800

    chore(config): update layout, header, and dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {Footer} from "@/components/footer";
+import { Footer } from "@/components/footer";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -21,20 +22,16 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
-}>) {
-    return (
-        <html lang="en">
-            <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-            >
-                <main className="flex-grow">
-                    {children}
-                </main>
-                <Footer/>
-            </body>
-        </html>
-    );
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <Toaster position="top-right" reverseOrder={false} />
+      </body>
+    </html>
+  );
 }
