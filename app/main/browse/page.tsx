239d9bb2c4f83a725337614f70aa7c6cab8c07ae
Kate Liu <lhyhgshyd@163.com>
2025-11-29 00:06:07 -0800

    fix: wrap browse page with Suspense to fix useSearchParams prerender error

--- app/main/browse/page.tsx
+++ app/main/browse/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import BrowseItemsContent from "./BrowseItemsContent";
 
-export default function Page() {
-  return <BrowseItemsContent />;
+export default function BrowsePage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <BrowseItemsContent />
+    </Suspense>
+  );
 }
